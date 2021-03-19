8f8605608ea816e7b5057a5adb210f78f7d39077
Mitchell <soggybag@gmail.com>
2021-03-19 12:41:46 -0700

    updated

--- class-6/websockets/index.js
+++ class-6/websockets/index.js
@@ -1,3 +1,4 @@
+// Get references to DOM elements
 const sendBtn = document.querySelector('#send');
 const messages = document.querySelector('#messages');
 const messageInput = document.querySelector('#message-input');
@@ -5,28 +6,35 @@ const messageInput = document.querySelector('#message-input');
 let ws;
 
 function showMessage(message) {
-  messages.textContent += `\n\n${message}`;
+  messages.innerHTML += `${message}\n\n`;
   messages.scrollTop = messages.scrollHeight;
   messageInput.value = '';
 }
 
 function init() {
+  // Clean up before restarting a websocket connection
   if (ws) {
     ws.onerror = ws.onopen = ws.onclose = null;
     ws.close();
   }
 
+  // Make a new Websocket
   ws = new WebSocket('ws://localhost:6969');
+  // Handle the connection when it opens
   ws.onopen = () => {
     console.log('Connection opened!');
   }
+  // handle a message event
   ws.onmessage = ({ data }) => showMessage(data);
+  // Handle a close event
   ws.onclose = function () {
     ws = null;
   }
 }
 
+// Handle button clicks
 sendBtn.onclick = function () {
+  // Send a message
   if (!ws) {
     showMessage("No WebSocket connection :(");
     return;
--- class-6/websockets/server.js
+++ class-6/websockets/server.js
@@ -10,9 +10,25 @@ const server = http.createServer(express);
 // Open a web socket
 const wss = new WebSocket.Server({ server })
 
-// Handle a 
+// wss.addListener('connection', (e) => {
+// 	console.log('**** Connection')
+// })
+
+// wss.addListener('message', (e) => {
+// 	console.log('???', e.data)
+// 	wss.clients.forEach((client) => {
+// 		if (client !== ws && client.readyState === WebSocket.OPEN) {
+// 			client.send(e.data);
+// 		}
+// 	})
+// })
+
+
+// Handle a web socket connection
 wss.on('connection', function connection(ws) {
 	// After making a connection start listening for messages
+	console.log('client connecting')
+
   ws.on('message', function incoming(data) {
 		// For each client broadcast the data
     wss.clients.forEach(function each(client) {
