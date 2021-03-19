7dc42be1f137e3e9203d47c5001de6483cfad7f6
Mitchell <soggybag@gmail.com>
2021-03-19 10:44:46 -0700

    added some notes

--- class-6/websockets/server.js
+++ class-6/websockets/server.js
@@ -1,14 +1,20 @@
-
+// Dependencies
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
+// Define a port
 const port = 6969;
+// create a server
 const server = http.createServer(express);
+// Open a web socket
 const wss = new WebSocket.Server({ server })
 
+// Handle a 
 wss.on('connection', function connection(ws) {
+	// After making a connection start listening for messages
   ws.on('message', function incoming(data) {
+		// For each client broadcast the data
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
@@ -17,6 +23,7 @@ wss.on('connection', function connection(ws) {
   })
 })
 
+// Start the server
 server.listen(port, function() {
   console.log(`Server is listening on ${port}!`)
 })
